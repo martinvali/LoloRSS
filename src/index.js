aacf31585ad4cade594e8cdfa7aabbe7bb040e65
Martin <martinvali12@gmail.com>
2024-05-16 14:25:06 +0300

    use node-fetch to fetch the url for data, send it to the front-end

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const app = require("./configs/server");
+const fetch = require("node-fetch");
 
 app.get("/", (req, res) => {
     res.sendFile("index.html");
@@ -6,8 +7,14 @@ app.get("/", (req, res) => {
 
 app.get("/test", (req,res) => console.log(req));
 
-app.get("/content", (req, res) => {
+app.get("/content", async (req, res) => {
     const url = req?.query?.url;
+    if(!url) return res.status(400).json("");
+
+    const response = await fetch(url);
+    const data = await response.text();
+    res.set('Content-Type', 'application/xml');
+    return res.send(data);
 });
 app.listen(3000, () => {
     console.log("Listening on port 3000");
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,14 +1,15 @@
 async function fetchInitialContent () {
     const INITIAL_CONTENT_URL = "https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss";
-    const initialData = await getUrlContent(INITIAL_CONTENT_URL);
+    const response = await getUrlContent(INITIAL_CONTENT_URL);
+    const data = await response.text();
+    console.log(data);
 }
 
 async function getUrlContent (url) {
     const queryStringParameters = new URLSearchParams();
     queryStringParameters.append("url", url);
     const response = await fetch("/content?" + queryStringParameters);
-    const data = await response.json();
-    return data;
+    return response;
 }
 
 fetchInitialContent();
