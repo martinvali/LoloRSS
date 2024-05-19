8682519064b8c74ad020056a036f83fd94904d1c
Martin <martinvali12@gmail.com>
2024-05-19 20:42:19 +0300

    add Poppins font via google fonts. Console.log news items´ titles

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -3,6 +3,7 @@ async function fetchInitialContent () {
     const response = await getUrlContent(INITIAL_CONTENT_URL);
     const data = await response.text();
     const xmlDocument = parseXml(data);
+    generateNewsArticles(xmlDocument);
 }
 
 async function getUrlContent (url) {
@@ -17,5 +18,17 @@ function parseXml (xmlString) {
     const document = parser.parseFromString(xmlString, "application/xml");
     return document;
 }
+
+function generateNewsArticles (xmlDocument) {
+    const newsItems = xmlDocument.getElementsByTagName("item");
+    console.log(xmlDocument);
+    
+    for (let i = 0; i < newsItems?.length; i++) {
+        const newsItem = newsItems[i];
+        const title = newsItem.getElementsByTagName("title")?.[0];
+        console.log(title);
+    }
+}
+
 
 fetchInitialContent();
