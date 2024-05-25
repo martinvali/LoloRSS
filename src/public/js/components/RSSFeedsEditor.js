cf9d93713b1de8c11c2d173239748300791fcc46
Martin <martinvali12@gmail.com>
2024-05-25 16:33:34 +0300

    add logic for adding new rss feed urls after the add button is clicked

--- src/public/js/components/RSSFeedsEditor.js
+++ src/public/js/components/RSSFeedsEditor.js
@@ -1,20 +1,33 @@
 export default class RSSFeedsEditor {
     #urls;
     #container;
+    #addNewFeedInput;
 
     constructor (urls, container) {
         this.#urls = urls;
         this.#container = container;
+        this.#addNewFeedInput = container.querySelector(".add-new-rss-feed-input");
+        container.querySelector(".add-new-rss-feed-button").addEventListener("click", this.#addNewFeed.bind(this));
         this.#generateInputsForAllUrls();
     }
 
     #generateInputsForAllUrls () {
         for (const url of this.#urls) {
-            const input = document.createElement("input");
+            this.#displayNewFeed(url);
+        }
+    }
+
+    #displayNewFeed (url) {
+        const input = document.createElement("input");
             input.type = "text";
             input.value = url;
             input.classList.add("rss-feed-input");
             this.#container.appendChild(input);
-        }
+    }
+
+    #addNewFeed () {
+        const url = this.#addNewFeedInput?.value;
+        this.#urls.push(url);
+        this.#displayNewFeed(url);
     }
 }
